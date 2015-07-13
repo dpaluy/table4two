7befdcd12ffaac5d0cc311549445bde64b1e7808
David Paluy <david@paluy.org>
2015-07-13 11:21:44 +0300

    contacts added

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -7,4 +7,13 @@ $(function() {
     $(document.body).on('appear', '.animated-content', function (e, $affected) {
       $(this).removeClass('animated-content').addClass('appeared');
     });
+
+    // mobile nav - open/close
+    $('#menu-open').click(function () {
+        $('body').addClass('mobile-nav-opened hide-overflowY');
+    });
+
+    $('#menu-close').click(function () {
+        $('body').removeClass('mobile-nav-opened hide-overflowY');
+    });
 });
